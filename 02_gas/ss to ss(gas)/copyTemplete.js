8e5ff357b0304364e64aea358829ae2a9b225472
TAKA-002 <66257816+TAKA-002@users.noreply.github.com>
2020-09-04 06:58:47 +0900

    実際のテストファイルの内容に更新（0904）

--- 02_gas/ss to ss(gas)/copyTemplete.js
+++ 02_gas/ss to ss(gas)/copyTemplete.js
@@ -1,13 +1,13 @@
 //仮想シフト表から「月を取得」してmm形式でリターン
 function getMakingMonth() {
   //仮想シフト表のIDを取得する
-  const ss = SpreadsheetApp.openById('1YWp9IUB1LGed4LZGYd2DNKvQx4RqSTozmWYs-hSiAWI');
+  const ss = SpreadsheetApp.openById('1DNxOMCa8u3qcZLVws9kvxh2TdLMr7rLdu4ffc7aLy0M');
   
   //取得したスプレッドシートのシフト表のシートを取得する
   const sheet = ss.getSheetByName('シート1');//シフト表のシート名にする
   
   //シフト表のシートの作成月の記載されているセルを取得する
-  const range = sheet.getRange('B3').getValue();//シフト表の年月日を入力しているセルにする
+  const range = sheet.getRange('B2').getValue();//シフト表の年月日を入力しているセルにする
   
   //Dateオブジェクトで指定したセルの値をインスタンスする
   var shiftDate = new Date(range);
@@ -19,9 +19,9 @@ function getMakingMonth() {
 
 //仮想シフト表から「年」を取得を取得してyyyy形式でリターン
 function getMakingYear() {
-  const ss = SpreadsheetApp.openById('1YWp9IUB1LGed4LZGYd2DNKvQx4RqSTozmWYs-hSiAWI');
+  const ss = SpreadsheetApp.openById('1DNxOMCa8u3qcZLVws9kvxh2TdLMr7rLdu4ffc7aLy0M');
   const sheet = ss.getSheetByName('シート1');
-  const range = sheet.getRange('B3').getValue();
+  const range = sheet.getRange('B2').getValue();
   var shiftDate = new Date(range);
   var year = shiftDate.getFullYear();
   return year;
@@ -34,14 +34,14 @@ function copyTemplete(){
   var makingYear = getMakingYear();
   
   //テンプレートとなる業務管理シートを取得
-  var templeteFile = DriveApp.getFileById('1lsxixwYUSTfWOStrcM2mmocPjgyFvNyTbj2B395T6WU');
+  var templeteFile = DriveApp.getFileById('1KbccBa1YAqPQpY_tAYFeVB2W8MfX4nIn1E6pgI97fY0');
   
   //出力先のフォルダーを取得
-  var OutputFolder = DriveApp.getFolderById('1ekDBmDsLyo8gqxxRenS3neNhz6Sg0W91');
+  var OutputFolder = DriveApp.getFolderById('19uX3BktgUEpZyNXd4COca31Mrc8iyEKt');
   
   //出力する時のファイル名を指示する
-  //  var OutputFileName = templeteFile.getName().replace('仮想業務管理シート', '')+Utilities.formatDate(new Date(), 'JST', 'yyyyMM');
-  var OutputFileName = templeteFile.getName().replace('仮想業務管理シート', '業務管理シート_' + makingYear + makingMonth);
+  //  var OutputFileName = templeteFile.getName().replace('（三浦）NHK-業務管理シート のコピー', '')+Utilities.formatDate(new Date(), 'JST', 'yyyyMM');
+  var OutputFileName = templeteFile.getName().replace('（三浦）NHK-業務管理シート のコピー', '業務管理シート_' + makingYear + makingMonth);
   
   //実行
   templeteFile.makeCopy(OutputFileName, OutputFolder);
