0dcb6203f19ddf245d89854a0c95225ff48928ee
TAKA-002 <66257816+TAKA-002@users.noreply.github.com>
2020-09-05 01:45:18 +0900

    微修正

--- 02_gas/ss to ss(gas)/copyTemplete.js
+++ 02_gas/ss to ss(gas)/copyTemplete.js
@@ -3,7 +3,7 @@
 //仮想シフト表から「月を取得」してmm形式でリターン
 function getMakingMonth() {
   //仮想シフト表のIDを取得する
-  const ss = SpreadsheetApp.openById('1YWp9IUB1LGed4LZGYd2DNKvQx4RqSTozmWYs-hSiAWI');//★★★①シフト表のIDを入力★★★
+  const ss = SpreadsheetApp.openById('1DNxOMCa8u3qcZLVws9kvxh2TdLMr7rLdu4ffc7aLy0M');//★★★①シフト表のIDを入力★★★
   
   //取得したスプレッドシートのシフト表のシートを取得する
   const sheet = ss.getSheetByName('シート1');//★★★②シフト表のシート名を入力★★★
@@ -22,7 +22,7 @@ function getMakingMonth() {
 
 //仮想シフト表から「年」を取得を取得してyyyy形式でリターン
 function getMakingYear() {
-  const ss = SpreadsheetApp.openById('1YWp9IUB1LGed4LZGYd2DNKvQx4RqSTozmWYs-hSiAWI');//★★★④シフト表のIDを入力★★★
+  const ss = SpreadsheetApp.openById('1DNxOMCa8u3qcZLVws9kvxh2TdLMr7rLdu4ffc7aLy0M');//★★★④シフト表のIDを入力★★★
   const sheet = ss.getSheetByName('シート1');//★★★⑤シフト表のシート名を入力★★★
   const range = sheet.getRange('B2').getValue();//★★★⑥シフト表の年月日を入力しているセルを入力★★★
   var shiftDate = new Date(range);
@@ -37,13 +37,13 @@ function copyTemplete(){
   var makingYear = getMakingYear();
   
   //テンプレートとなる業務管理シートを取得
-  var templeteFile = DriveApp.getFileById('1lsxixwYUSTfWOStrcM2mmocPjgyFvNyTbj2B395T6WU');//★★★⑦テンプレートの業務管理シートファイルIDを入力★★★
+  var templeteFile = DriveApp.getFileById('1KbccBa1YAqPQpY_tAYFeVB2W8MfX4nIn1E6pgI97fY0');//★★★⑦テンプレートの業務管理シートファイルIDを入力★★★
   
   //出力先のフォルダーを取得
-  var OutputFolder = DriveApp.getFolderById('1ekDBmDsLyo8gqxxRenS3neNhz6Sg0W91');//★★★⑧出力先フォルダーIDを入力★★★
+  var OutputFolder = DriveApp.getFolderById('19uX3BktgUEpZyNXd4COca31Mrc8iyEKt');//★★★⑧出力先フォルダーIDを入力★★★
   
   //出力する時のファイル名を指示する
-  var OutputFileName = templeteFile.getName().replace('仮想業務管理シート', '業務管理シート_' + makingYear + makingMonth);//★★★⑨replace('テンプレートのファイル名', '新ファイル名'）を入力★★★
+  var OutputFileName = templeteFile.getName().replace('（三浦）NHK-業務管理シート のコピー', '業務管理シート_' + makingYear + makingMonth);//★★★⑨replace('テンプレートのファイル名', '新ファイル名'）を入力★★★
   
   //実行
   templeteFile.makeCopy(OutputFileName, OutputFolder);
