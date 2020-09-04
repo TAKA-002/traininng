98013610705ca9dac9c54b4d7d3bcec5ae02863b
TAKA-002 <66257816+TAKA-002@users.noreply.github.com>
2020-09-04 19:58:09 +0900

    微修正（0904）

--- 02_gas/ss to ss(gas)/main.js
+++ 02_gas/ss to ss(gas)/main.js
@@ -1,12 +1,9 @@
 function myFunction() {
   //業務管理シートテンプレートファイルをコピー（copyTemplete.gs）
-    copyTemplete();
-    getDuplicatedFile();
+  copyTemplete();
   
   //複製した業務管理シートの中のテンプレートシートを複製（土日祝日と平日でことなるテンプレート（createSheet.gs）
-  
+  getDuplicatedFile();
   
   //シフト表の中のシフト(休・昼1・夜1・①など) を、それだった時に業務管理シートのプルダウンが対応するものに切り替わるようにする
-  
-  
 }
