7e23fdf3e036fcc7255914eb285710b29d87e46b
TAKA-002 <66257816+TAKA-002@users.noreply.github.com>
2020-09-04 19:56:18 +0900

    使用時の修正箇所の整理と微修正（0904）

--- 02_gas/ss to ss(gas)/createSheet.js
+++ 02_gas/ss to ss(gas)/createSheet.js
@@ -1,9 +1,8 @@
+//①〜⑤を状況に応じて変更
+
 //対象の月の日数を調べる
 function findLastDays() {
-    //シフト表の年を取得
     var year = getMakingYear();
-
-    //シフト表の月を取得
     var month = getMakingMonth();
 
     //シフト表の年月の最終日を取得
@@ -12,23 +11,19 @@ function findLastDays() {
 }
 
 
+//複製したファイルを取得してcreateSheet関数を実行
 function getDuplicatedFile(){
-    //シフト表の年を取得
     var year = getMakingYear();
-
-    //シフト表の月を取得
     var month = getMakingMonth();
 
     //ドライブの中の複製先のフォルダを指定し、その中のファイルを取得
-    var files = DriveApp.getFolderById('19uX3BktgUEpZyNXd4COca31Mrc8iyEKt').getFiles();
-    var fileName = '業務管理シート_' + year + month;
+    var files = DriveApp.getFolderById('1ekDBmDsLyo8gqxxRenS3neNhz6Sg0W91').getFiles();//★★★①複製先フォルダIDを入力★★★
+    var fileName = '業務管理シート_' + year + month;//★★★②新ファイル（複製したファイル）名★★★
 
     while(files.hasNext()){
       var file = files.next();//戻り値はFile型
       var duplicatedFileName = file.getName();
       var fileId = file.getId();
-      Logger.log(fileId);
-
       if(duplicatedFileName === fileName){
       createSheet(fileId, year, month);
       break;
@@ -37,6 +32,7 @@ function getDuplicatedFile(){
 }
 
 
+//テンプレートシートから曜日祝日にあわせたモノを選択してコピーする
 function createSheet(fileId, year, month) {
     var lastDay = findLastDays();
     var calJa = CalendarApp.getCalendarById('ja.japanese#holiday@group.v.calendar.google.com');
@@ -48,22 +44,22 @@ function createSheet(fileId, year, month) {
         var date = new Date(`${year}\/${month}\/${day}`);
 
         if (date.getDay() == 0 || date.getDay() == 6) {
-            Logger.log(date.getDay());
-            //スプレッドシートをアクティブにする
-            var tempWESheet = ss.getSheetByName('土日テンプレート改');
-            //土日のシートを取得して複製する
-            var copiedSheet = tempWESheet.copyTo(ss);
-            copiedSheet.setName(year + month + day);
+          //曜日が土日ならこちら
+          var tempWESheet = ss.getSheetByName('土日テンプレート改');//★★★③土日テンプレートシート名★★★
+          var copiedSheet = tempWESheet.copyTo(ss);
+          copiedSheet.setName(year + month + day);
         }
-//        if (calJa.getEventsForDay(date).length > 0) {
-//            var tempHDSheet =  ss.getSheetByName('祝日テンプレート改');
-//            var copiedSheet = tempHDSheet.copyTo(ss);
-//            copiedSheet.setName(year + month + day);
-//        }
+//      if (calJa.getEventsForDay(date).length > 0) {
+          //祝日ならこちら
+//        var tempHDSheet =  ss.getSheetByName('祝日テンプレート改');//★★★④祝日テンプレートシート名★★★
+//        var copiedSheet = tempHDSheet.copyTo(ss);
+//        copiedSheet.setName(year + month + day);
+//      }
         else{
-            var tempWDSheet = ss.getSheetByName('平日テンプレート改');
-            var copiedSheet = tempWDSheet.copyTo(ss);
-            copiedSheet.setName(year + month + day);
-        }
+          //平日ならこちら
+          var tempWDSheet = ss.getSheetByName('平日テンプレート改');//★★★⑤平日テンプレートシート名★★★
+          var copiedSheet = tempWDSheet.copyTo(ss);
+          copiedSheet.setName(year + month + day);
+       }
     }
 }
