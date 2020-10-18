e52d57589430b47ac06c6e3913d191d19866c436
Takayuki <6015abc@gmail.com>
2020-10-18 10:33:14 +0900

    10月13日報告版：コメント箇所を調整

--- 02_gas/ss-to-ss/backup.js
+++ 02_gas/ss-to-ss/backup.js
@@ -1,53 +1,52 @@
-//------▼▼コードの目的▼▼------
-//コンテナバインドで開いているファイル自体を複製する
-//複製したファイルのファイル名をリネームする
+//============================
+//コードの目的
+//============================
+//開いているファイル自体を複製する
+//自動で複製したファイルのファイル名をリネームする
 
-
-//------▼▼コード調整箇所▼▼------
-
+//============================
+//コード調整箇所
+//============================
 //①「業務管理シート」のファイルIDを記述する
-const MANAGEMENT_SHEET_ID = '1L5fqXV1iRkLWeSqe2z8dwTd7GKu2RGTR9G9RBcphPnU';
-
 //②複製したファイルを格納するフォルダを記述する
+
+//①
+const MANAGEMENT_SHEET_ID = '1L5fqXV1iRkLWeSqe2z8dwTd7GKu2RGTR9G9RBcphPnU';
+//②
 const OUTPUT_FOLDER_ID = '19uX3BktgUEpZyNXd4COca31Mrc8iyEKt';
 
-//------▲▲コード調整箇所▲▲------
 
-
+//実行関数
 function copy_this_file() {
     var year = targetYear();
     var month = targetMonth();
-
     var willBeCopiedFill = DriveApp.getFileById(MANAGEMENT_SHEET_ID); //①
-
     var outputFolder = DriveApp.getFolderById(OUTPUT_FOLDER_ID); //②
 
     //業務管理シートのファイルをコピーする
     willBeCopiedFill.makeCopy(willBeCopiedFill.getName() + '（' + year + month + '）', outputFolder);
 }
 
+
 function targetYear() {
     var date = new Date();
     var targetYear = date.getFullYear();
-
     //年をまたぐ時
     var month = date.getMonth() + 1;
     if (month === 1) {
         var targetYear = targetYear - 1;
     }
-
     return targetYear;
 }
+
 
 function targetMonth() {
     var date = new Date();
     //セルの月を２桁に統一する
     var targetMonth = ("0" + date.getMonth()).slice(-2);
-
     //1月に12月分を作成する時
     if (targetMonth === "00") {
         var targetMonth = 12;
     }
-
     return targetMonth;
 }
