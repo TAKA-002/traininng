205fee8a12f61d368ead55ec370f4d12fa13c2e9
Takayuki <6015abc@gmail.com>
2020-11-24 12:54:05 +0900

    11/23更新の際アップ（余計なディレクトリをアップしてしまったので削除）

--- 03_whiteboard/sortValues.js
+++ 03_whiteboard/sortValues.js
@@ -1,53 +1,56 @@
-//A列をkeyにしてB列をvalueの配列を作る
-//D列をkeyにしてE列をvalueの配列を作る
-
 //valueに「休」が入っている配列を削除する
 
 //残った配列をシフト表の7時から21時のシフトの順番にならべかえる
 
-//D列とE列をそれぞれ取得して連想配列を作成
-function makeMembers() {
 
+//===============================
+//F列G列：2行目-最終行を取得して、休みを除外した配列を作成する
+//===============================
 
-    //開いているシートを取得する
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
+function getNextShiftData() {
+    var SS = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = SS.getActiveSheet();
+    var nextShiftData = sheet.getRange(2,6,32,2).getValues();
 
-    //シフトデータ確認表のシートを取得
-    var getValuesSheet = ss.getSheetByName(SheetName);
+//    Logger.log(nextShiftData);
+//    Logger.log(nextShiftData[0]);//[木下,休]
+//    Logger.log(nextShiftData[0][0]);//木下
+//    Logger.log(nextShiftData[0][1]);//休
+//    Logger.log(nextShiftData.length);//32
 
-    //シフトデータ確認票の１行目D列の最終行までの値を取得 => Membersへ
-    var Members = getValuesSheet.getRange(1, 4, getValuesSheet.getLastRow()).getValues().flat();
-
-    Logger.log(Members);
-
-    return Members;
+    var Shift = [];
+    for(var i = 0; i < nextShiftData.length;i++){
+        if(nextShiftData[i][1] === "休"){
+            continue;
+        }
+        //「休」以外のシフトを配列Shiftに追加
+        Shift.push(nextShiftData[i]); 
+    }
+    return Shift;
+//    Logger.log(Shift);
 }
 
-//D列とE列をそれぞれ取得して連想配列を作成
-function makePositions() {
 
-    //開いているシートを取得する
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
+//===============================
+//残りの想定される問題
+//===============================
 
-    //シフトデータ確認表のシートを取得
-    var getValuesSheet = ss.getSheetByName(SheetName);
+//ホワイトボードにはるシフトの日が「平日」「祝日」「土日」のどれかをgoogleカレンダーから判断
+//３つのなかから判断したものの業務管理シートの「マスタ」のA2:B最終行のvalueを取得
+//B列の時間が早い順番にソート
+//ソートした配列をスプレッドシートに記載
+//記載されたシフトのvalueだけを配列で取得
+//取得した配列の上から順番に、その値を「休」を除外した配列Shiftから探す
+//ヒットしたらそのvalue(だれか)を取得して連想配列を作成
+//なかったら、空文字をいれる
+//新しい配列が完成したら、シートに出力
 
-    //同じくE列の最終行までの値を取得＝Positionsへ
-    var Positions = getValuesSheet.getRange(1, 5, getValuesSheet.getLastRow()).getValues().flat();
+function sortShiftData(){
 
-    Logger.log(Positions);
 
-    return Positions;
-}
 
-//配列を作成
-function test() {
-    let member = ['水野', '熊谷', '三浦'];
-    let position = ['マネージャー①', '朝リーダー', '夜リーダー'];
-    let hairetsu = member.reduce(function (hairetsu, field, index) {
-        hairetsu[position[index]] = field;
-        return hairetsu;
-    }, {});
 
-    Logger.log(hairetsu);
+
+
+
 }
